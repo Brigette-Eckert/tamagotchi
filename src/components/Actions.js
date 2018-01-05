3a533fc18e2c1e77d9c453902cce7d2e3f71bce0
Brigette Eckert <brigette.eckert@live.com>
2018-01-05 09:04:32 -0800

    added function to switch picture based on status when correlating button is clicked

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -4,31 +4,28 @@ import {ButtonToolbar, Button, DropdownButton, MenuItem} from 'react-bootstrap'
 import '../styles/components/Actions.css'
 
 class Actions extends React.Component{
-    render(){
-        return(
-
-            <div>
-                <ButtonToolbar id="actions__button_bar">
-                    <Button onClick={console.log("visit")} bsSize="large" bsStyle="primary"> Visit</Button>
-                    <Button onClick={console.log("feed")} bsSize="large" bsStyle="danger">Feed</Button>
-                    <Button onClick={console.log("play")} bsSize="large" bsStyle="info">Play</Button>
-                    <Button onClick={console.log("groom")} bsSize="large" bsStyle="warning">Groom</Button>
-                    <Button onClick={console.log("sleep")}bsSize="large" bsStyle="success"> Sleep </Button>
-                    <DropdownButton id="actions__dropdown_button" bsSize="large" bsStyle="link" className="actions__switch" title="Switch Pet">
-                        <MenuItem>
-                            Inactive Pet1
-                        </MenuItem>
-                        <MenuItem>
-                            Inactive Pet2
-                        </MenuItem>
-                    </DropdownButton>
-                </ButtonToolbar>
-            </div>
-        )
-
-     }
+  render(){
+    return(
+      <div>
+        <ButtonToolbar id="actions__button_bar">
+          <Button onClick={()=>{this.props.setPetStatus("visit")}} bsSize="large" bsStyle="primary"> Visit</Button>
+          <Button onClick={()=>{this.props.setPetStatus("feed")}} bsSize="large" bsStyle="danger">Feed</Button>
+          <Button onClick={()=>{this.props.setPetStatus("play")}} bsSize="large" bsStyle="info">Play</Button>
+          <Button onClick={()=>{this.props.setPetStatus("groom")}} bsSize="large" bsStyle="warning">Groom</Button>
+          <Button onClick={()=>{this.props.setPetStatus("sleep")}}bsSize="large" bsStyle="success"> Sleep </Button>
+          <DropdownButton id="actions__dropdown_button" bsSize="large" bsStyle="link" className="actions__switch" title="Switch Pet">
+            <MenuItem>
+              Inactive Pet1
+            </MenuItem>
+            <MenuItem>
+              Inactive Pet2
+            </MenuItem>
+          </DropdownButton>
+        </ButtonToolbar>
+      </div>
+    )
+  }
 }
 
 export default Actions;
-//TODO: Set button to switch state according to current
 //TODO: Set switch to correlate to pet Array;= - Bun = PETS[0], Pusheen = PETS[1], Biscuit = PETS[2]
--- src/components/PetContainer.js
+++ src/components/PetContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'moment';
 import Pet from './Pet.js';
 import Actions from './Actions.js';
 import Stats from './Stats.js';
@@ -7,33 +8,45 @@ import Info from './Info.js'
 import '../styles/components/PetContainer.css';
 
 class PetContainer extends Component {
-    constructor(props){
-        super(props);
-        console.log(this.props);
-        this.state ={
-            name: this.props.pet.name,
-            species: this.props.pet.species,
-            status: "visit",
-            fullness: 50,
-            happiness: 50,
-            hygiene: 50,
-            energy: 50,
-            age: 3, //Time since hatched
-        };
-        console.log(this.state);
-    }
-    render() {
-      let { name, species, status, age } = this.state;
-      let { pet } = this.props;
-        return (
-            <div className="App">
-                <Actions/>
-                <Info name={name} species={species} age={age}/>
-                <Pet name={name} status={status}/>
-                <Stats pet={pet}/>
-            </div>
-        );
-    }
+  constructor(props){
+    super(props);
+    console.log(this.props);
+    this.state ={
+      name: this.props.pet.name,
+      species: this.props.pet.species,
+      status: "visit",
+      fullness: 50,
+      happiness: 50,
+      hygiene: 50,
+      energy: 50,
+      hatchDate: new Date(),
+      age: 3
+      //now: new Moment(new Date()),
+      //age: hatchDate.from(now, true),
+    };
+    this.setPetStatus = this.setPetStatus.bind(this);
+  }
+
+  setPetStatus(status){
+    this.setState({
+      status: status
+    });
+  }
+
+
+
+  render() {
+    let { name, species, status, age } = this.state;
+    let { pet } = this.props;
+      return (
+          <div className="App">
+              <Actions setPetStatus={this.setPetStatus}/>
+              <Info name={name} species={species} age={age}/>
+              <Pet name={name} status={status}/>
+              <Stats pet={pet}/>
+          </div>
+      );
+  }
 }
 
 Info.propTypes = {
